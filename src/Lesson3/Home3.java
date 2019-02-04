9384566c63b7041511850192b7457a287bfc9325
Anna <a.i.za@mail.ru>
2019-02-04 22:55:01 +0300

    My home work

--- src/Lesson3/Home3.java
+++ src/Lesson3/Home3.java
@@ -5,14 +5,16 @@ package Lesson3;
  */
 public class Home3 {
     public static void main(String[] args) {
+
         double[] party = new double[6];
 
         party[0] = 40_000.30;
         party[1] = 33_100.50;
         party[2] = 26_220.60;
         party[3] = 38_000.20;
-        party[4] = 31_15.50;
+        party[4] = 31_150.50;
         party[5] = 24_340.30;
+
 
         System.out.println("зарплата командира = " + party[0] + " рублей.");
         System.out.println("зарплата пилота = " + party[1] + " рублей.");
@@ -20,20 +22,23 @@ public class Home3 {
         System.out.println("зарплата второго командира = " + party[3] + " рублей.");
         System.out.println("зарплата второго пилота = " + party[4] + " рублей.");
         System.out.println("зарплата второго командира = " + party[5] + " рублей.\n \n");
+    }
 
-        for (int i=0; i<party.length;i++) {
+    public static void calculate(double[] party) {
+        for (int i = 0; i < party.length; i++) {
             party[i] = party[i] * 0.93;
-        }
 
-        System.out.println("зарплата командира за вычетом 7% = " + party[0] + " рублей.");
-        System.out.println("зарплата пилота за вычетом 7% = " + party[1] + " рублей.");
-        System.out.println("зарплата инженера за вычетом 7% = " + party[2] + " рублей.");
-        System.out.println("зарплата второго командира за вычетом 7% = " + party[3] + " рублей.");
-        System.out.println("зарплата второго пилота за вычетом 7% = " + party[4] + " рублей.");
-        System.out.println("зарплата второго командира за вычетом 7% = " + party[5] + " рублей.");
+
+            System.out.println("зарплата командира за вычетом 7% = " + party[0] + " рублей.");
+            System.out.println("зарплата пилота за вычетом 7% = " + party[1] + " рублей.");
+            System.out.println("зарплата инженера за вычетом 7% = " + party[2] + " рублей.");
+            System.out.println("зарплата второго командира за вычетом 7% = " + party[3] + " рублей.");
+            System.out.println("зарплата второго пилота за вычетом 7% = " + party[4] + " рублей.");
+            System.out.println("зарплата второго командира за вычетом 7% = " + party[5] + " рублей.");
+        }
+    }
 
 
 // С комментариями я опаздала по времени, все давно решили эту задачу:) Р.S. Метод изменяет исходный массив)
 
-    }
 }
--- src/Lesson3/Home4.java
+++ src/Lesson3/Home4.java
@@ -5,15 +5,19 @@ package Lesson3;
  */
 public class Home4 {
     public static void main(String[] args) {
-        String[] gemers = {"Gleb", "Boris", "Ivan","Egor"};
-        String[] horseNames = {"Pegas", "Alfa", "Legenda","Shanhai"};
-        int[] sums = {300_000, 200_000, 1_500_000, 600_000};
-        String[] selectHorses = {"Alfa", "Legenda", "Shanhai","Pegas"};
+        String[] gemers = {"Gleb", "Boris", "Ivan", "Egor"};
+        String[] horseNames = {"Pegas", "Alfa", "Legenda", "Shanhai"};
+        String[] selectHorses = {"Alfa", "Legenda", "Shanhai", "Pegas"};
+    }
 
-        for(int i=0; i <  gemers.length; i++){
-            System.out.print(gemers[i]+ " сделал ставку на лошадь по кличке ");
+    public static void payout(String[] horseNames, String[] gemers, String[] selectHorses) {
+        int[] sums = {300_000, 200_000, 1_500_000, 600_000};
+
+        for (int i = 0; i < gemers.length; i++) {
+            System.out.print(gemers[i] + " сделал ставку на лошадь по кличке ");
             System.out.print(selectHorses[i] + " сумма выйграша сосавляет ");
-            System.out.print(sums[i] * 0.66+1+ "  ");
+            System.out.println(sums[i] * (i * 0.66 + 1) + " руб.");
         }
     }
+
 }
