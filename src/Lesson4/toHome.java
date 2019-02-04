c7269a3035efb0b1caa6bb658fe698f9dafed4d1
Anna <a.i.za@mail.ru>
2019-02-04 22:55:59 +0300

    My home work

---
+++ src/Lesson4/toHome.java
@@ -0,0 +1,27 @@
+package Lesson4;
+
+/**
+ * Created by Anna on 29.12.2018.
+ */
+public class toHome {
+    public static void main(String[] args) {
+        String[] Candidate = {"Ivan","Gleb","Egor"};
+        String[] CandidateCitizen = {" Russian"," Grek"," Russian"};
+        String[] CandidateGender = {" Man"," Man"," Man"};
+        int[] CandidateOld = {30,30,45};
+        String[] CandidateEducation = {" Higher"," Secondary"," Higher"};
+
+
+        String citizen = " Russian";
+        String gender = " Man";
+        int old = 30;
+        String education = " Higher";
+
+
+
+
+    }
+
+
+
+}
